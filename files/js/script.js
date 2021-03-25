0f3742d3963e88ca1ff25ec441a1bc4703bb0b7d
TimJones97 <t27.jones@connect.qut.edu.au>
2021-03-25 19:11:36 +1000

    Added custom nav menu on mobile

--- files/js/script.js
+++ files/js/script.js
@@ -63,88 +63,27 @@ function setCopyrightYear(){
 	var theDate = new Date(); 
 	$(".year").text(theDate.getFullYear());
 }
-//Need to add these into their own classes later
-function addLightNav(){
-  $(".navbar-default").removeClass('light');
-  $(".navbar-default").addClass('light_mobile');
-  $(".navbar-default").css("height", "57px");
-  $(".navbar-brand img").css('height', '50px');
-  $(".navbar-brand img").css('width', '50px');
-  $(".navbar-default").css("opacity", "1");
-  $(".navbar-nav li a").css("color", "#fff");
+function hideNavOnTap(){
+	$('.nav a').click(function() {
+	    $('.navbar-collapse').collapse('hide');
+	});
 }
-function addTransparentNav(){
-  //Make navbar transparent if scroll position is on main section
-  $(".navbar-default").removeClass('light_mobile');
-  $(".navbar-default").removeClass('light');
-  $(".navbar-default").css("border-top", "none");
-  $(".navbar-collapse").css("background-color", "none");
-  $(".navbar-nav li a").css("margin-top", "40px");
-  $(".navbar-default").css("height", "100px");
-  // if($(window).width() < 991){
-  //   $(".navbar-nav li:nth-of-type(2)").css("padding-left", "20px");
-  // }
-  // else {
-  //   $(".navbar-nav li:nth-of-type(2)").css("padding-left", "40px");
-  // }
-  $(".navbar-nav li a:hover").css("color", "#fff");
-  $(".navbar-nav li .navbar-brand").css("margin-top", "25px");
-}
-function addLightNavDesktop(){
-  $(".navbar-default").addClass('light');
-  $(".navbar-default").css("border-top", "none");
-  $(".navbar-default").css("display", "block");
-  $(".navbar-collapse").css("background-color", "none");
-  $(".navbar-nav li a").css("margin-top", "15px");
-  $(".navbar-default").css("height", "75px");
-  // $(".navbar-nav li:nth-of-type(2)").css("padding-left", "10px");
-  $(".navbar-nav li .navbar-brand").css("margin-top", "5px");
-  $(".navbar-brand img").css('height', '50px');
-  $(".navbar-brand img").css('width', '50px');
-}
-function animateNavbar(){
-  //If on mobile
-  var scrollCounter = $(document).scrollTop();
-  if($(window).width() < 767){
-    addLightNav();
+Pace.on("done", function(){
+  if ($('.pace-progress').attr('data-progress-text') == '100%'){
+  	setTimeout(function(){
+  		$('.pace').removeClass('pace-inactive');
+	    $('.cge_loader').fadeOut(1000);
+	    $('.preloader-white').fadeOut(1000);
+	    $('.pace').fadeOut(1000);
+  	}, 1000);
   }
-  //If on desktop
-  else {
-    //Scroll position is in About section
-    if(scrollCounter > 1) {
-      addLightNavDesktop();
-    }
-    //Scroll position is in Main section
-    else {
-      //Make navbar transparent if scroll position is on main section
-      addTransparentNav();
-    }
-  }
-}
-//Reset the elements that require resizing
-function setMainElements(){
-  $(".navbar-nav li a").removeAttr("style"); 
-
-  //Reset all styles on navbar if on desktop width
-  if($(window).width() > 767) {
-    $(".navbar-default").removeAttr("style");
-    // $(".navbar-nav li a").removeAttr("style");   
-    $(".navbar-collapse").removeAttr("style");
-    $(".navbar-collapse .navbar-nav").removeClass("transparent");
-  }
-  else {
-    $( ".navbar-nav li a" ).each(function( index ) {
-      $(this).css('width', '100%');
-    });
-  }
-}
+});
 $(document).ready(function(){
 	// Scroll to top so that WebGL ripple effect loads properly
 	$(document).scrollTop(0);
-  	setMainElements();
-  	animateNavbar();
 	smallNavOnScroll();
 	setCopyrightYear();
+	hideNavOnTap();
 	createCanvas();
 	new universalParallax().init({
 	    speed: 2.0
